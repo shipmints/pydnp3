e47b0d20bc2b163185c4203f289d5ff2699a478a
shipmints <shipmints@gmail.com>
2020-02-05 17:59:09 -0500

    pybind11 2.4.3 with copy_constructor allow copies patch

--- reset_pybind11_submodule.sh
+++ reset_pybind11_submodule.sh
@@ -1,7 +1,22 @@
-git rm --cached deps/pybind11
-rm -rf .git/modules/deps/pybind11
-rm -rf deps/pybind11
+#!/bin/bash
+
+set -x
+
+# manually purge submodule reference in .gitmodules
+# may need to purge submodule reference in .git/config
+# git reset HEAD .gitmodules deps/pybind11
+mod=pybind11
+branch=allow_pointer_copies
+url=https://github.com/shipmints/$mod.git
+git submodule deinit deps/$mod
+git rm --cached deps/$mod
+rm -rf .git/modules/deps/$mod
+rm -rf deps/$mod
 cd deps
-git submodule add https://github.com/Kisensum/pybind11.git
-git submodule update --init --recursive
+git submodule add -b $branch $url
+# cd $mod && git checkout $branch
 cd ..
+# git submodule update --init --recursive
+# git submodule update --init
+
+# EOF
